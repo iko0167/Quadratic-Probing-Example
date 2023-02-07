d2661051bd4bddc0e64bb180f41cf98cb3185398
ilksen <iekmekci67@gmail.com>
2023-02-07 17:22:56 +0300

    First Commit

---
+++ main.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int A[5] = {14, 8, 21, 11, 25};
+    int D[7] = {0, 0, 0, 0, 0, 0, 0};
+    int sizeA = sizeof(A) / sizeof(A[0]);
+    int sizeD = sizeof(D) / sizeof(D[0]);
+    int k, t;
+
+    for(int i = 0; i < sizeA; i++){
+        k = 1;
+        int x = A[i]%sizeD;
+        t= x;
+
+        while(D[x] != 0){
+            x = t;
+            x = x + ((k * k)%sizeD);
+            k = k + 1;
+        }
+
+        D[x] = A[i];
+
+        for(int j = 0; j < sizeD; j++)
+            printf("%d\t", D[j]);
+        printf("\n");
+    }
+
+    return 0;
+}
